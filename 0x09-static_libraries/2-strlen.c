800cb786868dc288c9d8d58abf2d62d048c571ae
Peterpet864 <Peterokon86@yahoo.com>
2022-11-06 12:07:35 -0800

    Ppet

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
+ * _strlen - returns the length of a string
  * @str: The string to get the length of.
- *
+ * @s: pointer to str
  * Return: The length of @str.
  */
+
 int _strlen(char *s)
-{
-}
