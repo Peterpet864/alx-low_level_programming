2c35d5c43f12a9e09856db2fa5187cb7004667fa
Peterpet864 <Peterokon86@yahoo.com>
2023-01-12 20:01:43 -0800

    link doubler

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at specified index in D.L.L
- * @h: Address of pointer to head node
- * @idx: index at which to add the new node
- * @n: data to be added to the D.L.L
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
  *
- * Return: address of new node (Success) or NULL (failed)
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *curr, *new_node;
-	unsigned int i = 0;
+	dlistint_t *tmp = *h, *new;
 
-	if (*h == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	curr = *h;
-	while (curr != NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		if (i == idx)
-		{
-			(curr->prev)->next = new_node;
-			new_node->prev = curr->prev;
-			curr->prev = new_node;
-			new_node->next = curr;
-			return (new_node);
-		}
-		curr = curr->next;
-		i++;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
-	free(new_node);
-	return (NULL);
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
